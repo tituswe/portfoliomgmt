939ec54ff35505becabdd60b273f1b6275a58ee9
Titus Lowe <tituslowesocial@gmail.com>
2025-08-08 13:26:22 +0800

    Add logos

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { SidebarTrigger } from "@/components/ui/sidebar";
+"use client";
+
+import Image from "next/image";
 import { ModeToggle } from "./mode-toggle";
+import { useTheme } from "next-themes";
 
 export function SiteHeader() {
+  const { resolvedTheme } = useTheme();
+
+  const isDarkMode = resolvedTheme === "dark";
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -12,7 +17,13 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         /> */}
-        <h1 className="text-base font-medium">MoneyStar Portfolio Management App</h1>
+        <Image
+          src={isDarkMode ? "/moneystar_dark.png" : "/moneystar_light.png"}
+          alt="Logo"
+          width={64}
+          height={40}
+          priority
+        />
         <div className="ml-auto flex items-center gap-2">
           <ModeToggle />
         </div>
